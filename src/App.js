79b9990b14b3d7bdc8f7bd625732274f574d66bc
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-22 16:15:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
+import { connect } from 'react-redux';
 import Restaurants from './components/Restaurants';
 
 export class App extends Component {
+  mapDispatchToProps = dispatch => {
+    return {
+      addRestaurant: () => {
+        dispatch(addRestaurant())
+      }
+    }
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,4 +22,3 @@ export class App extends Component {
   }
 };
 
-export default App;
